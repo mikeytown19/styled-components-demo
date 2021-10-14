1a07bde01f495ddda8b2f813fa14f8f4462230b9
mikeytown19 <mikeytown19@gmail.com>
2021-10-14 10:24:55 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-xe241x
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-xe241x)
---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './style.css';
+import styled from 'styled-components';
+const Button = styled.a`
+  display: inline-block;
+  border-radius: 3px;
+  padding: 10px 20px;
+  margin: 0.5rem 1rem;
+  background-color: ${({ color }) => color};
+  `;
+
+export default function App() {
+  const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      {[...Array(1000).keys()].map((item) => (
+        <Button color={'#' + Math.floor(Math.random() * 16777215).toString(16)}>
+          {' '}
+          Click Me{' '}
+        </Button>
+      ))}
+
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
